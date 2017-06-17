525df44c0ad8c7404054facd75cba15c8d616c1f
papadage <gpapadakis@aegis-net.com>
2017-06-16 20:02:36 -0400

    Practice typing commands after not studying for a while.

--- Welcome03/Welcome03.cs
+++ Welcome03/Welcome03.cs
@@ -1,13 +1,39 @@
-﻿
+﻿// Figure 3.11: Welcome03.cs
+// Display multiple lines with a single statement
+
 using System;
 
-namespace Welcome03
+class Welcome03
 {
-   class Welcome03
+   // Begin Main method
+   static void Main()
    {
-      static void Main()
-      {
-         Console.WriteLine($"Welcome\nto\nC#\nProgramming!");
-      }
-   }
-}
+      Console.WriteLine("Welcome\nto\nC#\nProgramming!");
+
+   } // End Main method
+} // End class Welcome
+
+
+//using System;
+
+//class Welcome03
+//{
+//   static void Main()
+//   {
+//      Console.WriteLine("Welcome\nto\nC#\nProgramming!");
+//   }
+//}
+
+
+//using System;
+
+//namespace Welcome03
+//{
+//   class Welcome03
+//   {
+//      static void Main()
+//      {
+//         Console.WriteLine($"Welcome\nto\nC#\nProgramming!");
+//      }
+//   }
+//}
